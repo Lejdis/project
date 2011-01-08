ed2a9232abf30e3ba2b09f85bd5cb26b9e204e37
Dorkis <dd.dorota@wp.pl>
2011-01-08 19:18:57 +0100

    walidacja

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -43,8 +43,22 @@ end
   # POST /films.xml
   def create
     @film = Film.new(params[:film])
+<<<<<<< HEAD
+	
+	    respond_to do |format|
+      if @film.save
+        format.html { respond_with(@film) }
+        format.xml  { render :xml => @film, :status => :created, :location => @film }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @film.errors, :status => :unprocessable_entity }
+      end
+    end
+	
+=======
     @film.save
     respond_with Film
+>>>>>>> ef25687cb52f2e47778fc37f12bf07aab7e3eb80
   end
 
   # PUT /films/1
